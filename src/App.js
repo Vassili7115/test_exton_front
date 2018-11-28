c17fd5a3407a52d96c7ba06daa6dd79c16a7c774
Matthieu <ranuccimatthieu@gmail.com>
2018-11-28 14:48:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+state={
+  data: null,
+}
+
+componentDidMount = () => {
+  fetch('http://localhost:3001/users/get')
+  .then(res => res.json())
+  // .then(data => console.log(data))
+  .then(data => this.setState({ data }))
+};
   render() {
+    const { data } = this.state;
+    if (data === null)
+      return <p>Loading ...</p>
+    console.log(data)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {data.map((x, i) =>
+        <div key={i}>
+          <p>{x.email}</p>
+          <p>{x.password}</p>
+          <p>{x.company_name}</p>
+        </div>)}
       </div>
     );
   }
